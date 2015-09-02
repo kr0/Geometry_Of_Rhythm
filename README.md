305c0133eb87cc9e9ca2978d4fee84876dcc5e69
kr0 <kelvin.jro@gmail.com>
2015-09-01 23:43:36 -0400

    Added file structure and rhythm class

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Geometry_Of_Rhythm
 A collection of java objects for the analysis of musical rhythm. Inspired by Godfried Toussaint's Geometry of musical rhythm.
+
+Buy his book or read the short paper form http://cgm.cs.mcgill.ca/~godfried/publications/geometry-of-rhythm.pdf 
---
+++ src/main/java/Rhythm.java
@@ -0,0 +1,57 @@
+/**
+ * A class representing a rhythm according to Toussaint's
+ * Geometry of musical rhythm.
+ * 
+ * @author kr0
+ *
+ */
+public class Rhythm {
+
+	private int numberOfPulses;
+	private char[] TUBS;
+	
+	
+	Rhythm(int _numberOfpulses){
+		numberOfPulses = _numberOfpulses;
+		TUBS = new char[numberOfPulses];
+	}
+	
+	
+	/**
+	 * Set onset on beat
+	 * @param beat
+	 */
+	public void putOnsetOnBeat(int beat){
+		TUBS[beat] = 1;
+	}
+	
+	/**
+	 * Excepts a string in Time Unit Box System notation.
+	 * e.g. [x.x.x..x]
+	 * @param tubString - a valid tubs strings
+	 */
+	public void putOnsetOnBeat(String tubString){
+		// TODO check that tubstring is valid
+		char[] cs = tubString.substring(1, tubString.length()-1).toCharArray(); 
+		for(int i = 0; i < cs.length; i++){
+			TUBS[i] = cs[i];
+		}
+	}
+	
+	public int getNumberOfPulses(){
+		return numberOfPulses;
+	}
+	
+	@Override
+	public String toString() {
+		return String.copyValueOf(TUBS);
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+}
